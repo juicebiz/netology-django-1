081bfa391334abd9916e4c22389192be5d621c80
Ivan Klyushin <juice_biz@mail.ru>
2023-03-23 16:41:39 +0300

    Homework ready

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+from os import listdir, path
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    msg = '<br>'.join([f for f in listdir('.') if path.isfile(f)])
+    return HttpResponse(msg)
